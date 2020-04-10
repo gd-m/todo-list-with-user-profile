4d1515a528357350f88987b61294dea5aba9a44d
gd-m <gitanjalidmule@gmail.com>
2020-04-10 01:22:38 -0500

    session coding initiated

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  
+  post '/api/v1/signup', to: 'users#create'
+  post '/api/v1/login', to: 'sessions#create'
+  delete '/api/v1/logout', to: 'sessions#destroy'
+  get '/api/v1/sessions', to: 'sessions#get_current_user'
+  
+  resources :users 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
